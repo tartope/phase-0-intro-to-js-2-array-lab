6bbfb2189518e6b53b240f123218a35e83071743
Tunisia Artope <artope@yahoo.com>
2022-01-14 19:35:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield",];
+function destructivelyAppendCat(name){
+cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const newCats = [...cats];
+    newCats.push("Broom");
+    return newCats;
+}
+function prependCat(name){
+    const newCats1 = [...cats];
+    newCats1.unshift("Arnold");
+    return newCats1;
+}
+function removeLastCat(){
+    const newCats2 = [...cats];
+    newCats2.pop();
+    return newCats2;
+}
+function removeFirstCat(){
+    const newCats3 = [...cats];
+    newCats3.shift();
+    return newCats3;
+}
