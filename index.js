322b4df05f2dbbcd09543237251810b9ed4d7822
Tunisia Artope <artope@yahoo.com>
2022-02-02 11:39:39 -0500

    commit tree 542c390f3ae9558fa6b67e26087c47c1b9578629

--- index.js
+++ index.js
@@ -1,34 +1,92 @@
-// Write your solution here!
-const cats = ["Milo", "Otis", "Garfield",];
+let cats = ["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendCat(name){
-cats.push("Ralph");
+    cats.push("Ralph")
 }
+
 function destructivelyPrependCat(name){
-    cats.unshift("Bob");
+    cats.unshift("Bob")
 }
+
 function destructivelyRemoveLastCat(){
-    cats.pop();
+    cats.pop()
 }
+
 function destructivelyRemoveFirstCat(){
-    cats.shift();
+    cats.shift()
 }
+
 function appendCat(name){
-    const newCats = [...cats];
+    let newCats = [...cats];
     newCats.push("Broom");
-    return newCats;
+    return newCats
 }
+
 function prependCat(name){
-    const newCats1 = [...cats];
-    newCats1.unshift("Arnold");
-    return newCats1;
+    let newCats = [...cats];
+    newCats.unshift("Arnold");
+    return newCats
 }
+
 function removeLastCat(){
-    const newCats2 = [...cats];
-    newCats2.pop();
-    return newCats2;
+    let newCats = [...cats];
+    newCats.pop();
+    return newCats
 }
+
 function removeFirstCat(){
-    const newCats3 = [...cats];
-    newCats3.shift();
-    return newCats3;
+    let newCats = [...cats];
+    newCats.shift();
+    return newCats
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write your solution here!
+// const cats = ["Milo", "Otis", "Garfield",];
+// function destructivelyAppendCat(name){
+// cats.push("Ralph");
+// }
+// function destructivelyPrependCat(name){
+//     cats.unshift("Bob");
+// }
+// function destructivelyRemoveLastCat(){
+//     cats.pop();
+// }
+// function destructivelyRemoveFirstCat(){
+//     cats.shift();
+// }
+// function appendCat(name){
+//     const newCats = [...cats];
+//     newCats.push("Broom");
+//     return newCats;
+// }
+// function prependCat(name){
+//     const newCats1 = [...cats];
+//     newCats1.unshift("Arnold");
+//     return newCats1;
+// }
+// function removeLastCat(){
+//     const newCats2 = [...cats];
+//     newCats2.pop();
+//     return newCats2;
+// }
+// function removeFirstCat(){
+//     const newCats3 = [...cats];
+//     newCats3.shift();
+//     return newCats3;
+// }
